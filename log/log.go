a2144705b234133eabe8966b2f1c43d626fa36fb
fayadehong <snackchina@163.com>
2023-02-08 19:10:42 +0800

    日志搭建

---
+++ log/log.go
@@ -0,0 +1,33 @@
+package logs
+
+import (
+	"github.com/lestrrat-go/file-rotatelogs"
+	"github.com/pkg/errors"
+	"github.com/rifflock/lfshook"
+	log "github.com/sirupsen/logrus"
+	"path"
+	"time"
+)
+
+func ConfigLocalFilesystemLogger(logPath string, logFileName string, maxAge time.Duration, rotationTime time.Duration) {
+	baseLogPath := path.Join(logPath, logFileName)
+	writer, err := rotatelogs.New(
+		baseLogPath+"-%Y%m%d%H%M.log",
+		// rotatelogs.WithLinkName(baseLogPath), // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(maxAge),             // 文件最大保存时间
+		rotatelogs.WithRotationTime(rotationTime), // 日志切割时间间隔
+	)
+	if err != nil {
+		log.Errorf("config local file system logger error. %+v", errors.WithStack(err))
+	}
+	lfHook := lfshook.NewHook(lfshook.WriterMap{
+		log.DebugLevel: writer, // 为不同级别设置不同的输出目的
+		log.InfoLevel:  writer,
+		log.WarnLevel:  writer,
+		log.ErrorLevel: writer,
+		log.FatalLevel: writer,
+		log.PanicLevel: writer,
+	}, &log.TextFormatter{DisableColors: true})
+	log.SetReportCaller(true) // 将函数名和行数放在日志里面
+	log.AddHook(lfHook)
+}
